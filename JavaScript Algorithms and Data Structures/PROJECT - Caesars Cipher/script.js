dc078a598ef6bb26d6dc3715ecfc07a575764e44
Luke Moule <lukemoule20@gmail.com>
2023-05-20 14:57:55 +0100

    Update script.js for Caesars Cipher

---
+++ JavaScript Algorithms and Data Structures/PROJECT - Caesars Cipher/script.js
@@ -0,0 +1,15 @@
+function rot13(str) {
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    let answer = ""
+    for (let i = 0; i < str.length; i++){
+      if (alphabet.indexOf(str[i]) >= 13){
+        answer += alphabet[alphabet.indexOf(str[i])-13]
+      } else if (alphabet.indexOf(str[i]) < 13 & alphabet.indexOf(str[i]) > -1){
+        answer += alphabet[alphabet.indexOf(str[i])+13]
+      } else {
+          answer += str[i]
+      }
+    }
+    return answer;
+  }
+  
